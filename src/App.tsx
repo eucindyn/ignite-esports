ed49d7af4d0ae9e9c36b5364e271cbe643bfedf4
eucindyn <cindy.nsc1@gmail.com>
2022-09-19 21:58:50 -0300

    :tada: first commit

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { GameBanner } from './components/GameBanner';
+import { CreateAdBanner } from './components/CreateAdBanner';
+import Logo from './assets/logo-nlw.svg';
+import './styles/main.css';
+
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  };
+}
+
+function App() {
+  const [games, setGames] = React.useState<Game[]>([]);
+
+  React.useEffect(() => {
+    fetch('http://localhost:3300/games')
+      .then((response) => response.json())
+      .then((data) => {
+        setGames(data);
+      });
+  });
+
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={Logo} alt="" />
+
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu{' '}
+        <span className="text-transparent bg-nlw-gradient bg-clip-text">
+          duo
+        </span>{' '}
+        está aqui.
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        {games.map((game) => {
+          return (
+            <GameBanner
+              key={game.id}
+              bannerUrl={game.bannerUrl}
+              title={game.title}
+              adsCount={game._count.ads}
+            />
+          );
+        })}
+      </div>
+
+      <CreateAdBanner />
+    </div>
+  );
+}
+
+export default App;

ae464f0946a55bf7934f6872ed90b1f2e745f512
eucindyn <cindy.nsc1@gmail.com>
2022-09-19 22:19:25 -0300

    criar modal com radix ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as Dialog from '@radix-ui/react-dialog';
 import { GameBanner } from './components/GameBanner';
 import { CreateAdBanner } from './components/CreateAdBanner';
 import Logo from './assets/logo-nlw.svg';
@@ -49,7 +50,19 @@ function App() {
         })}
       </div>
 
-      <CreateAdBanner />
+      <Dialog.Root>
+        <CreateAdBanner />
+
+        <Dialog.Portal>
+          <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
+
+          <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white">
+            <Dialog.Title>Publique um anúncio</Dialog.Title>
+
+            <Dialog.Content></Dialog.Content>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
     </div>
   );
 }
